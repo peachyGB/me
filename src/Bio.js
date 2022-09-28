81ccb8fe5eb66958f3b9a661826d936b19f1d8f1
Georgia Burnside <burnsidegeorgia@gmail.com>
2022-09-28 14:55:01 -0400

    tweak bio section

--- src/Bio.js
+++ src/Bio.js
@@ -9,12 +9,15 @@ function Bio() {
         I like to make things that tickle the mind and/or your funny bone. I
         also work to develop projects that are accesible and easy to use for all
         people. In my free time I like to play games, explore NYC parks with my
-        dog, and play music. As ive come to know myself throughout the years I
-        have found that I work well under pressure or what I like to refer to as
-        "grace under fire." I like to solve problems on LeetCocde, in web
-        development, or even in my friends relationships with their upstairs
-        neighbors. I love my work and I hope to share my passion with the people
-        I spend my days with.
+        dog, and play music.
+        <br />
+        <br />
+        As I've come to know myself throughout the years I have found that I
+        work well under pressure or what I like to refer to as "grace under
+        fire." I like to solve problems on LeetCocde, in web development, or
+        even in my friends relationships with their upstairs neighbors. I love
+        my work and I hope to share my passion with the people I spend my days
+        with.
       </p>
     </div>
   );
--- src/Skills.js
+++ src/Skills.js
@@ -10,14 +10,17 @@ import json from "./Images/json.png";
 import github from "./Images/github.png";
 
 function Skills() {
+  //renders icons relevant to each of my skill sets. (Still need to make Adobe and Avid icons in white. Need to keep that grid of 3s tho.)
   let imgFile = [html, css, javascript, react, rails, ruby, rest, json, github];
 
   return (
     <div>
       <h2>Skills</h2>
-      {imgFile.map((image) => (
-        <img src={image} className="skills-icon" />
-      ))}
+      <div className="skills-grid">
+        {imgFile.map((image) => (
+          <img src={image} className="skills-icon" />
+        ))}
+      </div>
     </div>
   );
 }
